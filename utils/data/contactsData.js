77f17726561093a2042de1245e69d73794e1dd82
unityengineer0101 <unityengineer0101@gmail.com>
2024-08-29 11:36:03 +0700

    Update contactsData.js

--- utils/data/contactsData.js
+++ utils/data/contactsData.js
@@ -1,6 +1,6 @@
 export const contactsData = {
     email: 'unityengineer0101@gmail.com',
-    phone: '+1 647 5588874',
+    phone: '+8562094090645',
     github: 'https://github.com/unityengineer',
     twitter: 'https://x.com/unityengineer0101',
     devUsername: "unityengineer0101"
